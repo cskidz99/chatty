18fc9c74bd357f1732e881b95f26865024f34118
Cameron Skidmore <cskidz99@gmail.com>
2016-11-08 19:17:50 -0700

    Finished project basic functionality.

--- server.js
+++ server.js
@@ -1,1 +1,54 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var app = express();
+app.use(bodyParser.json());
+
+var messages = [{
+  message: 'Welcome to the top-secret FBI database, sir.',
+  time: 'Zero-dark-thirty'
+  }];
+
+app.get('/', function( req, res ) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+  // console.log(messages);
+});
+
+app.post('/', function( req, res ) {
+
+    messages.push({
+      message: req.body.message,
+      time: new Date()
+    });
+
+    res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+
+});
+
+app.options('/', function(req, res, next){
+  res.status(200).set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  }).send(JSON.stringify(messages));
+})
+
+app.listen(8989,function(){
+  console.log('listening to secret transmissions...');
+})
